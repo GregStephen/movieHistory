f6ec158ab528ea3cd2330ac36bef44c239d7e4ab
Greg Stephen <GregAStephen@gmail.com>
2019-05-31 14:48:35 -0500

    styled cards better

--- src/javascripts/components/movies/movies.js
+++ src/javascripts/components/movies/movies.js
@@ -10,32 +10,40 @@ const movieStringBuilder = (uid) => {
       domstring += '</div>';
       domstring += '<div class="row justify-content-around mt-5">';
       movies.forEach((movie) => {
-        domstring += '<div class="col-12 col-md-6 col-lg-4">';
+        domstring += '<div class="d-flex col-12 col-md-6 col-lg-3 mb-4">';
         domstring += '<div class="card">';
-        domstring += `<img class="card-img-top" src="${movie.imageUrl}" alt="Poster for ${movie.title}">`;
+        domstring += '<div class="card-header">';
+        domstring += `<h4 class="movie-title">${movie.title}</h4>`;
+        domstring += '</div>';
+        domstring += `<img class="movie-poster card-img-top" src="${movie.imageUrl}" alt="Poster for ${movie.title}">`;
         domstring += '<div class="card-body">';
-        domstring += `<h2 class="card-title">${movie.title}</h2>`;
+        domstring += '<div class="row justify-content-between">';
+        domstring += '<div class="col-auto">';
         domstring += `<h6>${movie.releaseDate}</h6>`;
         domstring += `<h6>${movie.mpaaRating}</h6>`;
+        domstring += '</div>';
         if (movie.movieUserId === '') {
-          for (let n = 1; n < 6; n += 1) {
-            domstring += `<button type="button" value="${n}" id="${movie.id}" class="rateStar far fa-star fa-2x"></button>`;
-          }
-          domstring += '<div class="watchlistButtonDiv">';
+          domstring += '<div class="watchlistButtonDiv col-auto">';
           domstring += '<i class="fas fa-clock fa-2x"></i>';
           domstring += `<button id="${movie.id}"class="fas fa-plus fa-2x add-to-watchlist"></button>`;
           domstring += `<button id="${movie.movieUserId}"class="fas fa-check fa-2x hide remove-from-watchlist"></button>`;
           domstring += '</div>';
-        } else if (movie.isOnWatchList) {
+          domstring += '</div>';
           for (let n = 1; n < 6; n += 1) {
             domstring += `<button type="button" value="${n}" id="${movie.id}" class="rateStar far fa-star fa-2x"></button>`;
           }
-          domstring += '<div class="watchlistButtonDiv">';
+        } else if (movie.isOnWatchList) {
+          domstring += '<div class="watchlistButtonDiv col-auto">';
           domstring += '<i class="fas fa-clock fa-2x"></i>';
           domstring += `<button id="${movie.id}"class="fas fa-plus fa-2x hide add-to-watchlist"></button>`;
           domstring += `<button id="${movie.movieUserId}"class="fas fa-check fa-2x remove-from-watchlist"></button>`;
           domstring += '</div>';
+          domstring += '</div>';
+          for (let n = 1; n < 6; n += 1) {
+            domstring += `<button type="button" value="${n}" id="${movie.id}" class="rateStar far fa-star fa-2x"></button>`;
+          }
         } else if (movie.isWatched) {
+          domstring += '</div>';
           for (let i = 1; i < movie.rating + 1; i += 1) {
             domstring += `<button type="button" value="${i}" id="${movie.id}" class="rateStar fas fa-star fa-2x"></button>`;
           }
