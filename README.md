4d1bdf2db03f78cf7aed1169473b77636314fada
Greg Stephen <GregAStephen@gmail.com>
2019-06-15 10:22:22 -0500

    readme added, commented out useless user code

--- README.md
+++ README.md
@@ -1,1 +1,42 @@
-# movieHistory
+# Movie History
+
+## Description
+This is a fun little website that a user can add a movie to a database, rate movies they have watched and add or remove a movie from their database. Clicking the cast and crew button shows more detailed info about the movie.
+
+## Screenshots
+* Landing Page
+
+![Landing Page](./assets/screenshots/full_page.png)
+
+* Add A Movie
+
+![Add A Movie](./assets/screenshots/add_movie.png)
+
+* Reviewed and on the watchlist
+
+![Reviewed movie and on the watchlist](./assets/screenshots/reviewed_on_watchlist.png)
+
+* Unreviewed and not on watchlist
+
+![Unreviewed movie and not on the watchlist](./assets/screenshots/unreviewed_not_on_watchlist.png)
+
+* Watchlist Page
+
+![Watchlist Page](./assets/screenshots/watchlist.png)
+
+* Cast and Crew
+
+![Cast and Crew](./assets/screenshots/cast_and_crew.png)
+
+
+## Installation Instructions
+* Clone down this repo
+* At the root of the project, run `npm install`
+
+## How to run
+* In the terminal, type `npm start` to run the webpage.
+* If you want to make a production build of this project, type `npm run build`. This will create a folder called build with all of the minified code you need.
+
+
+## Author
+Greg Stephen
--- src/javascripts/helpers/data/authData.js
+++ src/javascripts/helpers/data/authData.js
@@ -1,15 +1,15 @@
 import firebase from 'firebase/app';
 import 'firebase/auth';
-import Axios from 'axios';
-import $ from 'jquery';
-import apiKeys from '../apiKeys.json';
+// import Axios from 'axios';
+// import $ from 'jquery';
+// import apiKeys from '../apiKeys.json';
 
 import movies from '../../components/movies/movies';
 import watchListButton from '../../components/watchlist/watchlist';
 import addMovieButton from '../../components/addMovie/addMovie';
 import rateMovieButtons from '../../components/rateMovie/rateMovie';
 
-const firebaseUrl = apiKeys.firebaseKeys.databaseURL;
+// const firebaseUrl = apiKeys.firebaseKeys.databaseURL;
 const moviesDiv = document.getElementById('movies');
 const authDiv = document.getElementById('auth');
 const moviesNavbar = document.getElementById('navbar-button-movies');
@@ -19,18 +19,18 @@ const logoutNavbar = document.getElementById('navbar-button-logout');
 const checkLoginStatus = () => {
   firebase.auth().onAuthStateChanged((user) => {
     if (user) {
-      Axios.get(`${firebaseUrl}/user.json?orderBy="uid"&equalTo="${user.uid}"`)
-        .then((resp) => {
-          if (Object.entries(resp.data).length === 0 && resp.data.constructor === Object) {
-            const newUser = {
-              uid: user.uid,
-              avatar: 'https://www.w3schools.com/howto/img_avatar.png',
-              name: 'New User',
-            };
-            Axios.post(`${firebaseUrl}/user.json`, newUser);
-            $('#newUserModal').modal('show');
-          }
-        }).catch(err => console.error('new user error', err));
+      // Axios.get(`${firebaseUrl}/user.json?orderBy="uid"&equalTo="${user.uid}"`)
+      //   .then((resp) => {
+      //     if (Object.entries(resp.data).length === 0 && resp.data.constructor === Object) {
+      //       const newUser = {
+      //         uid: user.uid,
+      //         avatar: 'https://www.w3schools.com/howto/img_avatar.png',
+      //         name: 'New User',
+      //       };
+      //       Axios.post(`${firebaseUrl}/user.json`, newUser);
+      //       $('#newUserModal').modal('show');
+      //     }
+      //   }).catch(err => console.error('new user error', err));
       authDiv.classList.add('hide');
       moviesDiv.classList.remove('hide');
       moviesNavbar.classList.remove('hide');
